7d94df58376212a4f223c78a164a77cd031bd8f8
SMPchase <chaselotharp3@gmail.com>
2025-05-20 01:35:06 -0400

    nice

--- script-2.js
+++ script-2.js
@@ -1,8 +1,11 @@
 
-setTimeout(() => {
+document.addEventListener('DOMContentLoaded', () => {
   const intro = document.getElementById('gifIntro');
-  intro.classList.add('fade-out');
+
   setTimeout(() => {
-    intro.style.display = 'none';
-  }, 2000);
-}, 10000);
+    intro.classList.add('fade-out');
+    setTimeout(() => {
+      intro.style.display = 'none';
+    }, 2000); // match the transition fade duration
+  }, 10000); // wait for GIF display duration (10 seconds)
+});
