75366e98a7f73d908e5ff734af4b3ae37db0f817
SMPchase <chaselotharp3@gmail.com>
2025-05-23 08:31:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,5 +7,5 @@ document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
       intro.style.display = 'none';
     }, 2000); // match the transition fade duration
-  }, 9000); // wait for GIF display duration (10 seconds)
+  }, 10000); // wait for GIF display duration (10 seconds)
 });
