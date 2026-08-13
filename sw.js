5e5f092218029fd0f2c4e043c663614f85fa05f7
SMPchase <chaselotharp3@gmail.com>
2026-08-13 07:36:37 -0400

    Add release-aware player and installable app

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = "saint-jules-shell-v2";
+const SHELL = [
+  "/",
+  "/manifest.webmanifest",
+  "/album-cover.jpeg",
+  "/Glowing Saint Jules Angel.png",
+  "/saint-jules-icon-192.png",
+  "/saint-jules-icon-512.png",
+  "/apple-touch-icon.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET" || event.request.destination === "audio") return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response.ok && new URL(event.request.url).origin === self.location.origin) {
+          const copy = response.clone();
+          void caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((response) => response || caches.match("/"))),
+  );
+});
